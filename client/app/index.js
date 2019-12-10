b615a9bcf171a9640ff614c045c9131f8c0f3352
ebonnecab <edotclaire@gmail.com>
2019-12-09 19:09:36 -0800

    added list of tags to js page

--- client/app/index.js
+++ client/app/index.js
@@ -121,10 +121,49 @@ queryInput.addEventListener("keyup", function (e) {
     }
 })
 // Creates autoCompleteList array by combining lists
-const autoCompleteList = ["item 1", "item 2", "item 3",
-    "thing 1", "thingy", "temperature", "precipitation", "humidity",
-    "water cover"
-]
+const autoCompleteList = ['quality', 'crop', 'elevation', 'ned', 'myd11a2', 'opengeohub', 'caltech', 'agriculture', 'google', 'mass', 'built', 'mangrove', 'aod',
+ 'ncep', 'dmsp', 'brightness', 'srtm', 'daylight', 'mcd64a1', 'myd13q1', 'l4', 'ciesin', 'forecast', 'coastal', 'hurricane', 'mcd43a4', 'state', 'bio5', 'ngdc', 
+ 'ocean', 'osb', 'nex', 'topography', 'l8', 'tm', 'photosynthesis', 'evapotranspiration', 'myd15a2h', 'sar', 'goes-17', 'black-sky', 'ges-disc', 'albedo', 'lai', 
+ 'avhrr', 'lt5', 'cas', 'nesdis', 'water-mask', 'cfmask', 'infrastructure', 'wind', 'mcd12q2', 'mod17a2', 'esa', 'vapor', 'canada', 'kbdi', 'lo8', 'nir', 'snow', 
+ 'lm1', 'elevation', 'eos', 'us', 'weather', 'imerg', 'ga', 'myd13a1', 'methane', 'fapar', 'cropland', 'oisst', 'seasonality', 'aerosol', 'pdsi', 'annual', 
+ 'surface', 'alos', 'emc', 'planet', 'aster', 'c1', 'production', 'county', 'moisture', 'soil', 'mod13a1', 'atmosphere', 'le7', 'accessibility', 'mydocga', 
+ 'nlcd-derived', 'vnir', 'bio19', 'netherlands', 'goes-west', 'dos', 'census', 'toms', 'smod', 'nasa', 'goes-east', 'worldpop', 'usda', 'hydrosheds', 'gap', 
+ 'l8sr', 'nws', 'tern', 'glims', 'ice', 'cdr', 'mod08-m3', 'so2', 'multispectral', 'taxonomy', 'mcd43a2', 'gfsad', 'myd13a2', 'msi', 'ospo', 'myd09q1', 'climate', 
+ 'stressors', 'mcd19a2', 'fragmentation', 'geophysical', 'hydrology', 'bio7', 'emissivity', 'boundaries', 'oli-tirs', 'fpar', 'brdf', 'australia', 'lm2', 'countries', 
+ 'surface-temperature', 'ndvi', 'eroc', 'night', 'pml', 'hyperspectral', 'csiro', 't2', 'classification', 'bioclim', 'idaho', 'gravity', 'jpl', 'bio17', 'crs', 
+ 'infrared', 'glacier', 'cgiar', 'goddard', 'counties', 'modocga', 'formaldehyde', 'corine', 'rainfall', 'famine', 'fishing', 'air-temperature', 'pantropical', 
+ 'hapludalfs', 'epa', 'hyperion', 'nopp', 'tropical', 'sst', 'annual', 'mod13q1', 'isothermality', 'rema', 'prism', 'psn', 't1', 'gldas', 'worldclim', 'bio2', 
+ 'l1', 'forest', 'uvai', 'tellus', 'yearly', 'fapar', 'mod44w', 'smoothed', 'clc', 'eea', '', 'gtopo30', 'ndvi', 'lt04', 'bio1', 'surface-reflectance', 'diurnal',
+  'antarctica', 'texture', 'biome', 'etm', 'daymet', 'ch4', 'water', 'tir', 'bio12', 'gimms', 'landforms', 'visible', '4-day', 'warmest', 'avnir-2', 'ahn', 'bio3', 
+  'optical', 'wetness', 'lidar', 'sea-ice', 'toa', 'power-plants', 'bulk', 'tier2', 's5p', 'lc08', 'maca', 'nlcd', 'reanalysis', 'vegetation', 'landcover', 'evi', 
+  'density', 'eosdis', 'srtm', 'fmask', 'polar', 'humidity', 'igpb', 'mod10a1', 'runoff', 'aafc', 'nass', 'demography', 'envirometrix', 'bio9', 'temperature', 
+  'history', 'oregonstate', 'highres', 'lights', 'recurrence', 'tiger', 'water', 'protected', 'ldas', 'gfw', 'reflectance', 'co', 'hourly', 'npp', 
+  'evapotranspiration', 'twente', 'biomass', 'slope', 'atmospheric', 'l2', 'uofi', 'shrub', 'rtma', 'mask', 'oli', 'sulfur-dioxide', 'mcd12q1', 'mod09q1', 
+  'boundaries', 'goes', 'wwf', 'fundamental', 'sentinel', 'sr', 'rt', 'vegetation', 'mod11a2', 'landsat-derived', 'intertidal', 'forests', 'pathfinder', 
+  'olci', 'organic', 'shrubland', 'mod09a1', 'lst-derived', 'visibility', 'geoscience-australia', 'probav', 'visible', 'political', 'gpw', 'soil-depth', 
+  'atmosphere', 'human-modification', 'vito', 'blm', 'digital-soil-mapping', 'tree-cover', 'bio13', 'pollution', 'bio14', 'viirs', 'rgb', 'naip', 'arctic', 
+  'myd11a1', 'social', 'cdl', 'sar', 'swir', 'stray-light', 'jrc', 'geophysical', 'myd09a1', 'canopy', 'antarctica', 'mod08', 'eorc', 'nrt', 'wildfire', 'nhc', 
+  'demographic', 'backscattering', 'mcd43a1', 'ergo', 'gridmet-derived', 'biodiversity', 'zipcode', 'fldas', 'vnp09ga', 'aspect', 'seawifs', 'dem', 'gfz', 'eu', 
+  'mrlc', 'us', 'no2', 'accumulation', 'radiance', 'argillic', 'tree', 'jaxa', 'aai', 'murray', 'hansen', 'precipitation', 'nfdrs', 'gridmet', 'ncar', 'umn', 
+  'proba', 'tirs', 'alos', 'chlorophyll', 'precipitable', 'drought', 'bio16', 'heat', 'skysat', 'mascon', 'mod13a2', '3-hourly', 'myd14a2', 'bira', 'lst', 'smos',
+   'oxford', 'metdata', 'biology', 'nitrogen-dioxide', 'lidar', 'nighttime', 'carbon', 'openlandmap', 'hotspot', 'nrcan', 'glcf', 'neighborhood', 'forma', 
+   'myd09gq', 'velocity', 'landsat', 'fpar', 'roads', 'bio10', 'oceandata', 'protection', 'gross-primary-product', 'pdsi', 'maiac', 'glas', 'air-quality', 
+   'ecoregions', 'slga', 'zcta', 'albedo', 'friction', 'usgs', 'hsl', 'era5', 'water-temp', 'palmer', 'pressure', 'globalsoilmap', 'grace', 'uq', 'gimp', 
+   'hydrography', 'tidal-flats', 'tasseled-cap', 'isccp', 'bio15', 'mod09ga', 'terra', 'nldas', 'eog', 'cpom', 'nsidc', 'ozone', 'copernicus', 'borders', 
+   'lt8', 'usdos', 'gfs', 'dem', 'mod14a1', 'tcb', 'productivity', 'white-sky', 'iucn', 'hcho', 'height', 'ocean', 'settlements', 'monitoring', 'le07', 'goes-r', 
+   'daily', 'lt4', 'zip', 'dlr', 'sr', 'fireburning', '30-year', 'onset-greenness', 'metop', 'fire', 'ged', 'radiance', 'surface-temperature', 'o3', 'tier1', 
+   'alos2', 'gls', 'wettest', 'flux', 'half-hourly', 'surface', 'real-time', 'landfire', 'gpm', 'mcd45a1', 'builtup', 'climate', 'thermal', 'lm5', 'cloud', 
+   'forcing', 'modis', 'whoi', 'sand', 'clay', 'cdem', 'evi', 'population', 'forest', 'states', 'wdpa', 'ucsb', 'lc8', 'mod16a2', 'myd10a1', 'urban', 'pgc', 
+   'whrc', 'utokyo', 'evaporation', 'soil-moisture', 'ciesin-derived', 'tropomi', 'precipitation', 'flow', 'carbon-monoxide', 'ecmwf', 'day', 'ornl', 'calibrated',
+    'ssh', 'fire', 'csp', 'myd14a1', 'emissivity', 'productivity', 'vnp13a1', 'dewpoint', 'radar', 'mcd115a3h', 'aura', 'gddp', 'radarsat-1', 'goes-s', 'global', 
+    'city', 'berkeley', 'bio18', 'temperature', 'fsa', 'landscape-gradient', 'snow', 'palsar', 'monthly', 'ucb', 'wtlab', 'chg', 'drainage', 'global', 'myd08-m3',
+    'map', 'lm3', 'burn', 'mss', 'watershed', 'wri', 'l5', 'glad', 'soil', 'radiation', 'deforestation', 'gpp', 'multitemporal', 'driest', 'lima', 'cryosat-2', 
+    'etopo1', 'lt05', 'firms', '8-day', 'polarization', 'arctic', 'persiann', 'watercontent', 'bio4', 'knmi', 'sulphur-dioxide', 'mod09gq', 'coldest', 
+    'landcover', 'ph', 'omi', 'resolve', 'hycom', 'csu', 'conservation', 'osu', 'umd', 'mod17', 'mosaic', 'conditioned', 'smap', 'orthorectified', 'bio8', 
+    'dnb', 'land', 'imagery', 'human', 'ncdc', 'crop', 'lance', 'greenland', 'soil-temperature', 'cmip5', 'sron', 'bedrock', 'conus', 'aot', 'goes-16', 'igsnrr',
+    'noaa', 'lst', 'mod17a3', 'wbd', 'sst', 'wind', 'void-filled', 'gpp', 'bathymetry', '16-day', 'mod44b', 'mod14a2', 'habitats', 'ols', 'aboveground', 'bio11', 
+    'globcover', 'eo-1', 'marine', 'mod15a2h', 'l7', 'myd09ga', 'mcd43b3', 'energy', 'direction', 'aqua', 'land', 'pansharpened', 'lai', 'phenology', 'power', 
+    'aerial', 'geostationary', 'ledaps', 'abi', 'l3', 'bio6', 'salinity', 'mcd43a3', 'wcmc', 'poes', 'mpa', 'tnc', 'potential', 'trmm', 'lm4', 'mod11a1']
 // auto completes queryInput with above array
 autocomplete(queryInput, autoCompleteList);
 
